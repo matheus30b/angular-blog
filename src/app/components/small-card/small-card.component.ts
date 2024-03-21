b2aa8c224416393304f7d0de5264f6d43e98513c
matheus30b <matheus_souza_penna@hotmail.com>
2024-03-20 21:07:49 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent implements OnInit {
 
   @Input()
   cardTitle:string=""
+  @Input()
+  Id:string="0"
 
   constructor() { }
 
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description":"Marvel Anuncia Novo Homem De Ferro",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=webp",
+
+  },
+  {
+    "id":"2",
+    "title": "Nova Série Anunciada No Disney +",
+    "description":"Bla bla bla",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+  }
+]
